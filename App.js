e12f277b200e674f168249b025598dee3cd102b3
tyl <tyliong@gmail.com>
2018-03-29 16:23:47 +0800

    Commit

--- App.js
+++ App.js
@@ -15,15 +15,11 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  test(){
+
+  }
 
   fetchData(){
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -51,24 +47,4 @@ export default class App extends Component<Props> {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('App', ()=>App);
--- __tests__/App.js
+++ __tests__/App.js
@@ -1,9 +1,14 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import {App} from '../App';
+// const myModule = require('../App');
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+beforeEach(() => {
+  test();
+});
 
 // it('renders correctly', () => {
 //   const tree = renderer.create(
@@ -15,11 +20,11 @@ test('renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
-test('the data is peanut butter', done => {
-  function callback(data) {
-    expect(data).toBe('peanut butter');
-    done();
-  }
-
-  App.fetchData(callback);
-});
+// test('the data is peanut butter', done => {
+//   function callback(data) {
+//     expect(data).toBe('peanut butter');
+//     done();
+//   }
+//
+//   // fetchData(callback);
+// });
